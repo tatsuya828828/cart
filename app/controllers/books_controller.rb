38a0154ab0d0925e56a680f32b0b147e63f357a1
oe0315 <oeoe0315@gmail.com>
2020-03-13 15:36:48 +0900

    [update]４章課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,18 +3,28 @@ class BooksController < ApplicationController
   end
 
   def index
+  	@book = Book.new
   end
 
   def show
   end
 
   def new
+  	@book = Book.new
   end
 
   def create
+  	books = Book.new(book_params)
+  	book.save
+  	reditect_to books_path
   end
 
   def edit
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 
 
