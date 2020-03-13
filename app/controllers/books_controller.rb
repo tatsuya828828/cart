f0982be5ee9984dfa3571fd332dd51d87044f285
oe0315 <oeoe0315@gmail.com>
2020-03-13 17:26:53 +0900

    [update]4章課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
 
   def index
   	@book = Book.new
+  	@books = Book.all
   end
 
   def show
@@ -14,9 +15,9 @@ class BooksController < ApplicationController
   end
 
   def create
-  	books = Book.new(book_params)
+  	book = Book.new(book_params)
   	book.save
-  	reditect_to books_path
+  	redirect_to books_path
   end
 
   def edit
