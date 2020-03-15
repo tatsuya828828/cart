91e16af3b9886c8c7ad5e1daeb0f2a9025fccaad
oe0315 <oeoe0315@gmail.com>
2020-03-16 05:41:29 +0900

    [finish]4章課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,7 @@ class BooksController < ApplicationController
   def create
   	@book = Book.new(book_params)
   	if @book.save
+      flash[:notice] = 'successfully'
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -30,9 +31,14 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to book_path(book.id)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+      flash[:notice] = 'successfully'
+  	  redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def destroy
