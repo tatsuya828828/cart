c536d4f482320cd421a8e4077ce59a63106fa027
oe0315 <oeoe0315@gmail.com>
2020-03-15 23:21:20 +0900

    [update]4章課題

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,13 +16,29 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+  	@book = Book.new(book_params)
+  	if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render 'index'
+    end
   end
 
   def edit
-  	@blog = Blog.find(params[:id])
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book.id)
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
   end
 
   private
